2ea42946af96bb59017981d2f423d2f7ae7930d5
gitikakhira69 <gitikakhira69@gmail.com>
2021-03-23 17:45:07 +0530

    C30

--- sketch.js
+++ sketch.js
@@ -1,13 +1,32 @@
+var mario,mario_running,mario_collide;
+var bg,bg_image;
 function preload(){
-
+  bg_image = loadImage("images/bgnew.jpg");
+  mario_running = loadAnimation("images/mar1.png","images/mar2.png","images/mar3.png","images/mar4.png",
+  "images/mar5.png","images/mar6.png","images/mar7.png");
 }
 
 function setup() {
 createCanvas(1000, 600);
-
-
+//backgroung image
+bg = createSprite(500,300);
+bg.addImage(bg_image);
+bg.scale=.5;
+//mario
+mario = createSprite(200,500,20,20);
+mario.addAnimation("running",mario_running);
+mario.scale=.3;
+//platform
+platform = createSprite(200,600,300,10);
 }
 
 function draw() {
-
+    //gravity to mario
+    mario.velocityY = mario.velocityY + 1 ;
+    //mario on platform
+    mario.collide(platform)
+  if(keyDown("space")){
+      mario.velocityY  = -5; 
+  } 
+drawSprites()
 }
